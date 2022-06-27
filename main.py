89b150fb673b39562769256ead2ab03ade5cd595
ccr185 <camilo.correa-restrepo@univ-paris1.fr>
2022-06-27 15:23:29 +0200

    Added JSON examples from Hiba and added parser

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+"""
+This is the VariaMos Semantic Translator
+"""
+import json
+
+with open('/home/kaiser185/workspace/semantic_translator/json/vmos.json', 'r') as f:
+    array = json.load(f)
+
+print(array)
