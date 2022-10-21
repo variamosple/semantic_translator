3d217a1981740068bb0ef0d42f5a104377a187db
ccr185 <camilo.correa-restrepo@univ-paris1.fr>
2022-10-21 10:28:22 +0200

    add hlvl test updtae francisco stuff

---
+++ grammars/hlvl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from textx.metamodel import metamodel_from_file
+import time
+
+
+def parse_hlvl(constraints_str):
+    t1 = time.time()
+    hlvl_meta = metamodel_from_file('hlvl.tx')
+    t2 = time.time()
+    model = hlvl_meta.model_from_str(constraints_str)
+    # model = hlvl_meta.model_from_file('e_shop2.hlvl', debug=False)
+    t3 = time.time()
+    print(vars(model))
+    print('T Model', t2-t1)
+    print('T Parse', t3-t2)
+    print('T Total', t3-t1)
